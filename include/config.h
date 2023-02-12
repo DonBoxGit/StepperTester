7986937b2777d08374bd4c5d9211072fed558cea
DonBoxGit <donbox@yandex.ru>
2023-02-13 01:08:48 +0300

    Add ONE_STEP_MEANDR define

--- include/config.h
+++ include/config.h
@@ -7,8 +7,8 @@
 #define ENBL_PIN 5
 
 /*-------------------------Encoder Connection Pins---------------------------*/
-#define DT  2         // Alternate notation S1
-#define SLK 3         // Alternate notation S2
+#define DT  3         // Alternate notation S1
+#define SLK 2         // Alternate notation S2
 #define SW  4         // Alternate notation Key
 
 /*---------------------Speaker Settings Connection Pins----------------------*/
@@ -20,11 +20,11 @@
 /*--------------------Button and Sensor Connection Pins----------------------*/
 #define BUTTON_RIGHT_PIN    A0
 #define BUTTON_LEFT_PIN     A1
-#define TOGLE_RIGHT_PIN     8
-#define TOGLE_LEFT_PIN      11
-#define BUTTON_RESET_PIN    10
-#define TERM_SW_PIN_1       9
-#define TERM_SW_PIN_2       12
+#define TOGLE_RIGHT_PIN     10
+#define TOGLE_LEFT_PIN      12
+#define BUTTON_RESET_PIN    11
+#define TERM_SW_PIN_1       A2
+#define TERM_SW_PIN_2       8
 
 /*----------------------Oled SSD1306 Display Settings------------------------*/
 #define OLED_RESET         -1
@@ -36,6 +36,12 @@
 #define T1_DURATION         10    // Duration pause time after Anable ON (min. 5 microSeconds)
 #define T2_DURATION         10    // Duration pause time after Direction Change (min. 5 microSeconds)
 #define T3_PULSE_WIDTH      5     // Duration time click pulse width (min. 2.5 microSeconds)
-#define T3_TIME_COUNTS      3     // Time_Counts = Target_Time / (Time_Resolution - 1)
+#define T3_TIME_COUNTS      3     // Time_Counts = Target_Time / Time_Resolution - 1
+
+/*-----------------Specification of MITSUMI M42SP-7 Motor--------------------*/
+#define MIN_PULSE           40
+
+/*---------------------Miscellaneous constant project------------------------*/
+#define ONE_STEP_MEANDR     10
 
 #endif /* _CONFIG_H_ */
