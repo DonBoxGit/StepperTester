e8d3ac53957fe5ec5252282941c297b446e9f841
DonBoxGit <donbox@yandex.ru>
2023-03-09 02:11:39 +0300

    added STEP_ANGLE_EXTERNAL define.

--- include/config.h
+++ include/config.h
@@ -1,21 +1,23 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
-/*----------------------Stepper Motor Connection Pins-----------------------*/
+/*--------------------| Stepper Motor Connection Pins |---------------------*/
 #define STEP_PIN 6
 #define DIR_PIN  7
-#define ENBL_PIN 5
-/* Pins for set microsteps */
-#define MS1      9
+#define ENBL_PIN_INTERNAL 5
+#define ENBL_PIN_EXTERNAL 9
+
+/* Pins for set microsteps need to define MICROSTEP_MODE_ENABLE */
+#define MS1      20
 #define MS2      17
 #define MS3      13
 
-/*-------------------------Encoder Connection Pins---------------------------*/
+/*-----------------------| Encoder Connection Pins |-------------------------*/
 #define SLK 2         // Alternate notation S2
 #define DT  3         // Alternate notation S1
 #define SW  4         // Alternate notation Key
 
-/*--------------------Button and Sensor Connection Pins----------------------*/
+/*------------------| Button and Sensor Connection Pins |--------------------*/
 #define BUTTON_RIGHT_PIN    A0
 #define BUTTON_LEFT_PIN     A1
 #define TOGLE_RIGHT_PIN     10
@@ -24,23 +26,24 @@
 #define TERM_SW_PIN_1       A2
 #define TERM_SW_PIN_2       8
 
-/*----------------------Oled SSD1306 Display Settings------------------------*/
+/*--------------------| Oled SSD1306 Display Settings |----------------------*/
 #define OLED_RESET         -1
 #define DISPLAY_I2C_ADDR    0x3C
 #define DISPLAY_WIDTH       128
 #define DISPLAY_HEIGHT      64
 
-/*----------------------Specification of Drivers A4988-----------------------*/
+/*--------------------| Specification of Drivers A4988 |---------------------*/
 #define T1_DURATION         10    // Duration pause time after Anable ON (min. 5 microSeconds)
 #define T2_DURATION         10    // Duration pause time after Direction Change (min. 5 microSeconds)
 #define T3_PULSE_WIDTH      5     // Duration time click pulse width (min. 2.5 microSeconds)
 #define T3_TIME_COUNTS      3     // Time_Counts = Target_Time / Time_Resolution - 1
 
-/*-----------------Specification of MITSUMI M42SP-7 Motor--------------------*/
-#define MIN_PULSE           40
-#define STEP_ANGLE          7.5F
+/*---------| Specification of MITSUMI M42SP-7 and External Motors |----------*/
+#define MIN_PULSE               40
+#define STEP_ANGLE_INTERNAL     7.5F
+#define STEP_ANGLE_EXTERNAL     1.8F
 
-/*---------------------Miscellaneous constants project-----------------------*/
+/*-------------------| Miscellaneous constants project |---------------------*/
 #define ONE_STEP_MEANDR     10
 
 #endif /* _CONFIG_H_ */
